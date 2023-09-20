5037695efd22010b8916a185080c2a54f3d7f381
joele89 <32238188+joele89@users.noreply.github.com>
2023-09-21 08:44:54 +1000

    decompress

--- decoder.js
+++ decoder.js
@@ -1,11 +1,14 @@
 
+
 async function decode(uploader) {
     if (uploader.files && uploader.files[0]&& (uploader.files[0].name.substring(uploader.files[0].name.lastIndexOf('.') + 1).toLowerCase() == "cca")) 
     var parseViewer = document.getElementById("parseViewer");
     var reader = new FileReader();
     var fileData = await new Promise(resolve => {
         reader.onload = function(e) { resolve(e.target.result); }
-        reader.readAsDataURL(uploader.files[0]);
+        reader.readAsArrayBuffer(uploader.files[0]);
     })
-    parseViewer.innerText = fileData
+    var decompressor = new DecompressionStream("gzip");
+    var decompressed = fileData.stream().pipeThrough(decompressor);
+    parseViewer.innerText = decompressed.readAsText()
 }
