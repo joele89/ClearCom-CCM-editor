b7319f1efb6e6b7e648a9054a8abef537d83e77b
joele89 <32238188+joele89@users.noreply.github.com>
2023-09-22 18:41:52 +1000

    tar decoders having issues, reverting

--- decoder.js
+++ decoder.js
@@ -1,4 +1,6 @@
 //uploader = document.getElementById("upload")
+var globalOffset;
+
 async function decode(uploader) {
     if (uploader.files && uploader.files[0]&& (uploader.files[0].name.substring(uploader.files[0].name.lastIndexOf('.') + 1).toLowerCase() == "cca")) 
     var parseViewer = document.getElementById("parseViewer");
@@ -9,7 +11,68 @@ async function decode(uploader) {
     })
     var decompressor = new DecompressionStream("gzip");
     var decompressed = new Blob([fileData]).stream().pipeThrough(decompressor);
-    var tarReader = new tarball.TarReader();
-    var tar = await tarReader.readFile(decompressed);
+    var reader = decompressed.getReader();
+    var fileText = "";
+    for (i = 0; fileBytes = (await reader.read()).value; fileBytes)
+    {
+        fileText += new TextDecoder("utf-8").decode(fileBytes);
+    }
+    globalOffset = 0;
+    //USTAR file format
+    var dataset = {};
+    do {
+        console.log("=======================")
+        var tableName = readString(fileText, 100);
+        var fileMode = readString(fileText, 8);
+        var ownerUID = readString(fileText, 8);
+        var groupGID = readString(fileText, 8);
+        var size = parseInt(readString(fileText, 12), 8);
+        var lastChange = parseInt(readString(fileText, 12), 8);
+        var checksum = readString(fileText, 7);
+        var typeFlag = readString(fileText, 2);
+        var linkedFilename = readString(fileText, 100);
+        var ustarIndicator = readString(fileText, 6);
+        var ustarVersion = readString(fileText, 2);
+        var ownerName = readString(fileText, 32);
+        var groupName = readString(fileText, 32);
+        var deviceMajorNumber = readString(fileText, 8);
+        var deviceMinorNumber = readString(fileText, 8);
+        var filenamePrefix = readString(fileText, 155);
+        var padding = readString(fileText, 12);
+        if (bodyLen > 0) {
+            var bodyLen = (Math.trunc(size / 512) + 1) * 512
+            var body = readString(fileText,bodyLen)
+        }
+        dataset[tableName] = {
+            fileMode: fileMode,
+            ownerUID: ownerUID,
+            groupGID: groupGID,
+            size: size,
+            lastChange: lastChange,
+            checksum: checksum,
+            typeFlag: typeFlag,
+            linkedFilename: linkedFilename,
+            ustarIndicator: ustarIndicator,
+            ustarVersion: ustarVersion,
+            ownerName: ownerName,
+            groupName: groupName,
+            deviceMajorNumber: deviceMajorNumber,
+            deviceMinorNumber: deviceMinorNumber,
+            filenamePrefix: filenamePrefix,
+            table: JSON.parse('[' + body + ']')
+        }
+    } while (globalOffset < fileText.length);
 
+    console.log()
+    console.log("REMAINDER");
+    console.log()
+    console.log(fileText.substring(globalOffset));
+
+}
+
+function readString(body, len) {
+    var ret = body.substring(globalOffset,globalOffset+len).replace('\0+$','');
+    console.log(globalOffset +':' + len + ': "' + ret + '"')
+    globalOffset+=len;
+    return ret;
 }
